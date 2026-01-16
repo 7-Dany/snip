cadd2fbcc563e50f506b5272f5e016d65b6d9db1
7-Dany <ali.ali.ameen245@gmail.com>
2026-01-17 00:20:59 +0200

    feat: add ErrNotFound to handle entity not found error by repository

--- internal/domain/errors.go
+++ internal/domain/errors.go
@@ -3,6 +3,7 @@ package domain
 import "errors"
 
 var (
+	ErrNotFound      = errors.New("entity not found")
 	ErrEmptyName     = errors.New("name cannot be empty")
 	ErrEmptyTitle    = errors.New("title cannot be empty")
 	ErrEmptyLanguage = errors.New("language cannot be empty")
