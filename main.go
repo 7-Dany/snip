b86990edffb7c8dc860a55a254d604634840073f
7-Dany <ali.ali.ameen245@gmail.com>
2026-01-18 05:51:29 +0200

    feat(main): add start app

--- main.go
+++ main.go
@@ -2,22 +2,18 @@ package main
 
 import (
 	"os"
-	"time"
 
 	"github.com/7-Dany/snip/internal/cli"
 	"github.com/7-Dany/snip/internal/storage"
 )
 
 func main() {
-	cli.PrintLoading("Loading Config...")
 	config, err := cli.LoadConfig()
 	if err != nil {
 		cli.PrintError("Error loading config!" + err.Error())
 		os.Exit(1)
 	}
-	cli.PrintSuccess("Config loaded")
 
-	cli.PrintLoading("Loading Store...")
 	repos := storage.New(config.StoragePath)
 	err = repos.Load()
 	if err != nil {
@@ -26,11 +22,8 @@ func main() {
 	}
 	defer repos.Save()
 
-	cli.PrintSuccess("Store loaded")
-	time.Sleep(300 * time.Millisecond)
-	cli.ClearScreen()
-
 	cli.PrintLogo()
 
-	cli.Run(os.Args, repos)
+	app := cli.NewCLI(repos)
+	app.Run(os.Args)
 }
