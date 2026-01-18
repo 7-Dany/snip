639732451b32acc48ee1d3be68a5199ff5f24baa
7-Dany <ali.ali.ameen245@gmail.com>
2026-01-18 05:50:24 +0200

    refactor(cli): seprated commands for easier impl

--- internal/cli/commands.go
+++ internal/cli/commands.go
@@ -4,12 +4,39 @@ import (
 	"github.com/7-Dany/snip/internal/storage"
 )
 
-func Run(args []string, repos *storage.Repositories) {
+type CLI struct {
+	snippet  *SnippetCommand
+	category *CategoryCommand
+	tag      *TagCommand
+	help     *HelpCommand
+}
+
+func NewCLI(repos *storage.Repositories) *CLI {
+	return &CLI{
+		snippet:  NewSnippetCommand(repos),
+		category: NewCategoryCommand(repos),
+		tag:      NewTagCommand(repos),
+		help:     NewHelpCommand(repos),
+	}
+}
+
+func (cli *CLI) Run(args []string) {
 	if len(args) < 2 {
 		PrintError("no command provided")
-		PrintHelp("")
+		cli.help.Print("")
 		return
 	}
 
-	PrintInfo(args[1])
+	switch args[1] {
+	case "help":
+		cli.help.manage(args[2:])
+	case "snippet":
+		cli.snippet.manage(args[2:])
+	case "category":
+		cli.category.manage(args[2:])
+	case "tag":
+		cli.tag.manage(args[2:])
+	default:
+		cli.snippet.manage(args[1:])
+	}
 }
