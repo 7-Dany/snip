5bdb0fdd42ffba715e3d2d10750c04527265a2bc
7-Dany <ali.ali.ameen245@gmail.com>
2026-01-18 02:51:14 +0200

    refactor: go fmt and vet result

--- main.go
+++ main.go
@@ -1,7 +1,36 @@
 package main
 
-import "fmt"
+import (
+	"os"
+	"time"
+
+	"github.com/7-Dany/snip/internal/cli"
+	"github.com/7-Dany/snip/internal/storage"
+)
 
 func main() {
-	fmt.Println("hello world")
+	cli.PrintLoading("Loading Config...")
+	config, err := cli.LoadConfig()
+	if err != nil {
+		cli.PrintError("Error loading config!" + err.Error())
+		os.Exit(1)
+	}
+	cli.PrintSuccess("Config loaded")
+
+	cli.PrintLoading("Loading Store...")
+	repos := storage.New(config.StoragePath)
+	err = repos.Load()
+	if err != nil {
+		cli.PrintError("Error loading repos!" + err.Error())
+		os.Exit(1)
+	}
+	defer repos.Save()
+
+	cli.PrintSuccess("Store loaded")
+	time.Sleep(300 * time.Millisecond)
+	cli.ClearScreen()
+
+	cli.PrintLogo()
+
+	cli.Run(os.Args, repos)
 }
